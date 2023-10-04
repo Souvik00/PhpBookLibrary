b86e0b207206340398ae8dd608a8fc4a55a65a40
Souvik00 <71000257+Souvik00@users.noreply.github.com>
2023-10-04 17:22:50 +0600

    Colors added

--- home.php
+++ home.php
@@ -1,33 +1,49 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Home</title>
 </head>
-<body>
-    <h1>Home</h1>
+<body style="background-color: rgb(134, 11, 139);">
+    
+    <h1 style="text-align: center; background-color:red;">Home</h1>
+    <div style="background-color: rgb(134, 11, 139); display: flex; flex-direction: column; align-items: center;">
+        <img src="books.jpg" alt="Book" style="width: 200px; height: 300px; ">
+        <br>
     <style>
   table.tb { border-collapse: collapse; width:300px; }
   .tb th, .tb td { padding: 5px; border: solid 1px #777; }
   .tb th { background-color: lightblue;}
+  text-transform:capitalize
 </style>
-<table class="tb" id="mytable">
-        <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Price</th>
-            <th>Availability</th>
-        </tr>
+<table class="table table-striped table-dark">
+<thead>
+    <tr >
+      <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Price</th>
+            <th scope="col">Availability</th>
+     </tr>
+ </thead>
         <?php
+        $i=1;
         $books = json_decode(file_get_contents('books.json'), true);
         foreach ($books as $book) {
             echo "<tr>";
+            echo "<td>" . $i . "</td>";
             echo "<td>" . $book['title'] . "</td>";
             echo "<td>" . $book['author'] . "</td>";
             echo "<td>" ."$" . $book['price'] . "</td>";
             echo "<td>" . $book['availability'] . "</td>";
             echo "</tr>";
+            $i++;
         }
         ?>
     </table>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 <html>
 <head>
     <title>Index</title>
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body>
-
-    <h1>Index</h1>
-    <div style="background-color: rgb(113, 67, 113); display: flex; flex-direction: row; align-items: center;">
-        <img src="books.jpg" alt="Book" style="width: 200px; height: 300px; margin: 20px;">
-    <button onclick="window.location.href='home.php'">Home</button>
-    <button onclick="window.location.href='insert.html'">Insert</button>
-    <button onclick="window.location.href='delete.html'">Delete</button>
-    <button onclick="window.location.href='update.html'">Update</button>
+<body style="background-color: rgb(134, 11, 139);">
+    
+    <h1 style="text-align: center; background-color:red;">Index</h1>
+    <div style="background-color: rgb(134, 11, 139); display: flex; flex-direction: column; align-items: center;">
+        <img src="books.jpg" alt="Book" style="width: 200px; height: 300px; ">
+        <br>
+    <button type="button" class="btn btn-success" onclick="window.location.href='home.php'">Home</button><br>
+    <button type="button" class="btn btn-warning" onclick="window.location.href='insert.html'">Insert</button><br>
+    <button type="button" class="btn btn-danger" onclick="window.location.href='delete.html'">Delete</button><br>
+    <button type="button" class="btn btn-info" onclick="window.location.href='update.html'">Update</button><br>
 </div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
