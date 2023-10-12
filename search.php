cdf840f1fae98246c56e52078c818f34fbc90051
Souvik00 <71000257+Souvik00@users.noreply.github.com>
2023-10-12 06:44:04 +0600

    search modified

--- search.php
+++ search.php
@@ -32,6 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $books = json_decode(file_get_contents('Souvik Sanyal - books.json'), true);
     $title = $_POST['title'];
  $i=1;
+ $flag=false;
     foreach ($books as $key => $book) {
         if ($book['title'] === $title) {
             echo "<tr>";
@@ -44,12 +45,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo "<td>" . $book['isbn'] . "</td>";
             echo "</tr>";
             $i++;
+            $flag=true;
         }
+        
     }
-    exit;
+    
+    
 }
 ?>
     </table>
+    <?php
+    if(!$flag)echo"No books found";
+    ?>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
